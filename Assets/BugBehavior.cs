a9b52d01b8e1269e5dbfbfa4995d2d94103d1f32
Nathan Anderson <SniperSmiley@gmail.com>
2021-08-07 12:30:42 -0700

    Updated Bug

--- Assets/BugBehavior.cs
+++ Assets/BugBehavior.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class BugBehavior : MonoBehaviour
 {
     public float speed = 1.0f;
+    public float chargeSpeed = 3.0f;
     public float walkDist = 5.0f;
     public float attackDistance = 3.0f;
     public float waitTime = 3.0f;
     public float centered = 0.1f;
+    public GameObject player;
 
     private float currSpeed = 1.0f;
     private Vector2 startingPosition;
     private Animator animator;
     private Rigidbody2D rb2d;
+    private Rigidbody2D playerRb2d;
     private Vector2 direction;
     private bool goingBack = true;
+    private bool charging = false;
+    private bool charge = false;
     private float elapsed = 0.0f;
     private float angle = 0.0f;
     void Awake()
@@ -23,23 +28,44 @@ public class BugBehavior : MonoBehaviour
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         startingPosition = rb2d.transform.position;
         animator = gameObject.GetComponent<Animator>();
-
+        playerRb2d = player.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        Vector2 curr = new Vector2(rb2d.transform.position.x, rb2d.transform.position.y);
+        Vector2 playerCurr = new Vector2(playerRb2d.transform.position.x, playerRb2d.transform.position.y);
+        float distToPlayer = Vector2.Distance(playerRb2d.transform.position, curr);
         elapsed += Time.deltaTime;
-        if (elapsed>waitTime)
+        //Player close to Bug
+        if (!charging && distToPlayer < attackDistance)
         {
             elapsed = 0.0f;
-            animator.SetBool("Walking",!animator.GetBool("Walking"));
+            animator.SetBool("Charging", true);
+            charging = true;
         }
-        //Player close to Bug
+        if (elapsed > waitTime)
+        {
+                elapsed = 0.0f;
+            if (!charging)
+            {
+                animator.SetBool("Walking", !animator.GetBool("Walking"));
+            }
+            else if (!charge)
+            {
+                charge = true;
+            }
+            else
+            {
+                charge = false;
+                charging = false;
+                animator.SetBool("Charging", false);
+            }
+        }
         //Player kills Bug
 
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("BugWalk"))
         {
-            Vector2 curr = new Vector2(rb2d.transform.position.x, rb2d.transform.position.y);
             float diff = Vector2.Distance(startingPosition, curr);
             if (diff < centered && goingBack)
             {
@@ -58,6 +84,15 @@ public class BugBehavior : MonoBehaviour
         else if (animator.GetCurrentAnimatorStateInfo(0).IsName("AngryBug"))
         {
             //charge the player
+            if (!charge)
+            {
+                currSpeed = 0.00001f;
+            }
+            else
+            {
+                direction = (playerCurr - curr).normalized;
+                currSpeed = chargeSpeed;
+            }
         }
         else if (animator.GetCurrentAnimatorStateInfo(0).IsName("BugDie"))
         {
@@ -65,12 +100,13 @@ public class BugBehavior : MonoBehaviour
         }
         else if (animator.GetCurrentAnimatorStateInfo(0).IsName("BugIdle"))
         {
-            currSpeed = 0;
+            currSpeed = 0.0001f;
         }
     }
 
     private void FixedUpdate()
     {
         rb2d.velocity = direction * currSpeed;
+        gameObject.GetComponent<SpriteRenderer>().flipX = (rb2d.velocity.x < 0.0f);
     }
 }
